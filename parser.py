9cd319bf8c6e6a25353229f710733c9f8e04aa9b
Yaroslav <zymer464@gmail.com>
2024-08-09 04:39:15 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,61 +1,45 @@
-import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
-import time
+import json
 
 # Загрузка куков из файла
 with open('cookies.json', 'r') as file:
     cookies_list = json.load(file)
 
-# Настройки Selenium
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# Запуск Playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    context = browser.new_context()
 
-# URL для запроса
-url = 'https://pro.avito.ru/statistics'
+    # Установка куков
+    for cookie in cookies_list:
+        context.add_cookies([{
+            'name': cookie['name'],
+            'value': cookie['value'],
+            'domain': cookie['domain'],
+            'path': cookie['path'],
+            'secure': cookie['secure'],
+            'httpOnly': cookie['httpOnly'],
+            'expires': cookie.get('expiry')
+        }])
 
-# Открываем страницу
-driver.get(url)
+    page = context.new_page()
+    url = 'https://pro.avito.ru/statistics'
+    page.goto(url)
+    html = page.content()
 
-# Устанавливаем куки
-for cookie in cookies_list:
-    cookie_dict = {
-        'name': cookie['name'],
-        'value': cookie['value'],
-        'domain': cookie['domain'],
-        'path': cookie['path'],
-        'secure': cookie['secure'],
-        'httpOnly': cookie['httpOnly'],
-    }
-    if 'expirationDate' in cookie:
-        cookie_dict['expiry'] = int(cookie['expirationDate'])
-    driver.add_cookie(cookie_dict)
-
-# Перезагружаем страницу с установленными куки
-driver.get(url)
-time.sleep(5)  # Ждем загрузки страницы
-
-# Получаем HTML страницы
-html = driver.page_source
-
-# Закрываем драйвер
-driver.quit()
+    # Закрытие браузера
+    browser.close()
 
 # Создаем объект BeautifulSoup для парсинга HTML
 soup = BeautifulSoup(html, 'html.parser')
 
 # Ищем элемент h3 с нужными классами
-titles = soup.find('h3', class_='styles-module-root-DMJPn styles-module-root-ZXV3x styles-module-size_xl-VXlBo styles-module-size_xl_dense-o6oau styles-module-size_xl-Aa8Ae styles-module-size_dense-CoFzr stylesMarginDense-module-root-VEwa5 stylesMarginDense-module-header-xl-R4QFp styles-module-root_top-yS5t5 styles-module-margin-top_4-o6CRC')
+titles = soup.find_all('h3', class_='styles-module-root-DMJPn styles-module-root-ZXV3x styles-module-size_xl-VXlBo styles-module-size_xl_dense-o6oau styles-module-size_xl-Aa8Ae styles-module-size_dense-CoFzr stylesMarginDense-module-root-VEwa5 stylesMarginDense-module-header-xl-R4QFp styles-module-root_top-yS5t5 styles-module-margin-top_4-o6CRC')
 
 # Печатаем все найденные названия товаров
 for title in titles:
     print(title.text)
-
-
 
 # Ищем элементы с другой разметкой
 other_elements = soup.find_all('span', class_='styles-module-wrapper-oT74B')
@@ -63,14 +47,11 @@ other_elements = soup.find_all('span', class_='styles-module-wrapper-oT74B')
 coun = 0
 # Печатаем содержимое других элементов
 for element in other_elements:
-    coun+= 1
+    coun += 1
     if coun != 4:
         pass
     else:
         print(element.text)
-
-
-
 
 # Ищем элементы с другой разметкой
 other_elements1 = soup.find_all('p', class_='styles-module-root-ZXV3x styles-module-size_ms-fh7oj styles-module-size_ms_dense-qE7e9 styles-module-size_ms-OZwRo styles-module-textAlign_end-Q9dH6 styles-module-size_dense-CoFzr undefined stylesMarginDense-module-root-VEwa5')
